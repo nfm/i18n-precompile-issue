ee25828db956a4564e64faa93514352531bf77e2
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2014-08-26 12:10:41 +1000

    Initialize Rails 4.0.9 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestI18n::Application.config.secret_key_base = '4122d24924a631d4f4942ea2313c04a0b0a3e6bda1ef916aa78bb53e3eab47e685106f67b3468a51718c03e4647361e0760d96a87a200b6db7ea36ddd8738490'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestI18n::Application.config.session_store :cookie_store, key: '_test-i18n_session'
